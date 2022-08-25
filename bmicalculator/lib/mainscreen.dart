1b7b642738c53aae97bfc44a9595dfad33473ba7
anandsagar00 <80447047+anandsagar00@users.noreply.github.com>
2022-08-26 04:08:25 +0530

    made height widget

--- bmicalculator/lib/mainscreen.dart
+++ bmicalculator/lib/mainscreen.dart
@@ -2,19 +2,23 @@ import 'package:bmicalculator/genderWidgets.dart';
 import 'package:flutter/material.dart';
 import 'constants.dart';
 
+//these are the colors which is going to change when the user interacts with app
+//I've made all these app as global so that other widgets in other classes can interact with these var and can change them
+Color toBeChangedWhenTappedMale = kSecondaryUIColor;
+Color toBeChangedWhenTappedFemale = kSecondaryUIColor;
+int selected = 0; //this variable denotes which gender is selected
+Color colorMale = Colors.white70;
+Color colorFemale = Colors.white70;
+int height = 180;
+
+
+
 class MainScreen extends StatefulWidget {
   @override
   State<MainScreen> createState() => _MainScreenState();
 }
 
 class _MainScreenState extends State<MainScreen> {
-  //these are the colors which is going to change when the user interacts with app
-  Color toBeChangedWhenTappedMale = kSecondaryUIColor;
-  Color toBeChangedWhenTappedFemale = kSecondaryUIColor;
-  Color colorMale = Colors.white70;
-  Color colorFemale = Colors.white70;
-
-  int height = 180;
 
   @override
   Widget build(BuildContext context) {
@@ -43,6 +47,7 @@ class _MainScreenState extends State<MainScreen> {
                             toBeChangedWhenTappedFemale = kSecondaryUIColor;
                             colorMale = Colors.limeAccent;
                             colorFemale = Colors.white70;
+                            selected = 1;
                           });
                         },
                       ),
@@ -57,54 +62,93 @@ class _MainScreenState extends State<MainScreen> {
                             toBeChangedWhenTappedFemale = Color(0xFFFF2E63);
                             toBeChangedWhenTappedMale = kSecondaryUIColor;
                             colorMale = Colors.white70;
-
                             colorFemale = Colors.limeAccent;
+                            selected = 2;
                           });
                         },
                       ),
                     ),
                   ],
                 ),
-                Expanded(
-                  child: Container(
-                    margin: EdgeInsets.all(10),
-                    padding: EdgeInsets.all(10),
-                    decoration: BoxDecoration(
-                      borderRadius: BorderRadius.circular(20),
-                      color: kSecondaryUIColor,
-                    ),
-                    child: Column(
-                      mainAxisAlignment: MainAxisAlignment.center,
-                      crossAxisAlignment: CrossAxisAlignment.stretch,
-                      children: [
-                        Text(
-                          'HEIGHT',
-                          style: TextStyle(color: Colors.white70, fontSize: 20),
-                          textAlign: TextAlign.center,
-                        ),
-                        Row(textBaseline: TextBaseline.alphabetic,
-                          mainAxisAlignment: MainAxisAlignment.center,
-                          crossAxisAlignment: CrossAxisAlignment.baseline,
-                          children: [
-                            Text(
-                              height.toString(),
-                              style: TextStyle(
-                                  fontSize: 60,
-                                  fontWeight: FontWeight.w900,
-                                  color: Colors.white),
-                            ),
-                            Text('cm' ,style: TextStyle(color: Colors.white70,fontSize: 25),),
-                          ],
-                        ),
-
-                      ],
-                    ),
-                  ),
-                )
+                HeightWidget()
               ],
             ),
           ))),
     );
     ;
+  }
+}
+
+
+//This stateful Height Widget is responsible for the change in height , responsible for all the changes in slider
+class HeightWidget extends StatefulWidget {
+
+  @override
+  State<HeightWidget> createState() => _HeightWidgetState();
+}
+
+class _HeightWidgetState extends State<HeightWidget> {
+
+  @override
+  Widget build(BuildContext context) {
+    return Expanded(
+      child: Container(
+        margin: EdgeInsets.all(10),
+        padding: EdgeInsets.all(10),
+        decoration: BoxDecoration(
+          borderRadius: BorderRadius.circular(20),
+          color: kSecondaryUIColor,
+        ),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          crossAxisAlignment: CrossAxisAlignment.stretch,
+          children: [
+            Text(
+              'HEIGHT',
+              style: TextStyle(color: Colors.white70, fontSize: 20),
+              textAlign: TextAlign.center,
+            ),
+            Row(
+              textBaseline: TextBaseline.alphabetic,
+              mainAxisAlignment: MainAxisAlignment.center,
+              crossAxisAlignment: CrossAxisAlignment.baseline,
+              children: [
+                Text(
+                  height.toString(),
+                  style: kHeightWeightAgeTextStyle),
+                Text(
+                  'cm',
+                  style: TextStyle(
+                      color: Colors.white70, fontSize: 25),
+                ),
+              ],
+            ),
+            Slider(
+              value: height.toDouble(),
+              onChanged: (double valueOnSlider) {
+                setState(() {
+                  height = valueOnSlider.round();
+                });
+              },
+              min: 100,
+              max: 220,
+              thumbColor: (selected == 0)
+                  ? Colors.white70
+                  : (selected == 1
+                  ? toBeChangedWhenTappedMale
+                  : toBeChangedWhenTappedFemale),
+              activeColor: (selected == 0)
+                  ? Colors.white70
+                  : (selected == 1
+                  ? toBeChangedWhenTappedMale
+                  : toBeChangedWhenTappedFemale),
+              inactiveColor: (selected == 0)
+                  ? Colors.white70
+                  : Colors.limeAccent,
+            ),
+          ],
+        ),
+      ),
+    );
   }
 }
